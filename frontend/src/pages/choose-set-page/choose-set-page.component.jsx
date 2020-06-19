c32b3d222c33e836f66760518ad672e1aa1a14d2
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-06-19 19:53:07 +0200

    Styles in choosing state ready

--- frontend/src/pages/choose-set-page/choose-set-page.component.jsx
+++ frontend/src/pages/choose-set-page/choose-set-page.component.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
 import './choose-set-page.styles.css'
 
-import {LearningSetBox} from '../../components/learning-set-box/learning-set-box.component'
+import { LearningSetBox } from '../../components/learning-set-box/learning-set-box.component'
 
 export const ChooseSetPage = () => (
     <div className="ChooseSetPage-container">
         <title className="ChooseSetPage-title">Choose learning set</title>
 
-        <h3>Continue</h3>
-        <LearningSetBox title_and_date={"Arbeit Wortschatz, from 21.04.2020"} progress={"67%"}></LearningSetBox>
+        <div className="ChooseSetPage-types">
+            <div className="ChooseSetPage-continue-column">
+                <h3 className="ChooseSetPage-continue">Continue</h3>
+                <div className="ChooseSetPage-box">
+                    <LearningSetBox title_and_date={"Arbeit Wortschatz, from 21.04.2020"} progress={"67%"} />
+                </div>
+                <div className="ChooseSetPage-box">
+                    <LearningSetBox title_and_date={"Arbeit Wortschatz, from 21.04.2020"} progress={"67%"} />
+                </div>
+
+            </div>
+
+            <div className="ChooseSetPage-begin-new-column">
+                <h3 className="ChooseSetPage-begin-new">Begin new</h3>
+
+                <div className="ChooseSetPage-begin-new-row">
+                    <div className="ChooseSetPage-begin-new-wortschatz-column">
+                        <h5 className="ChooseSetPage-wortschatz">Wortschatz</h5>
+                        <div className="ChooseSetPage-box">
+                            <LearningSetBox title_and_date={"Arbeit Wortschatz"} />
+                        </div>
+                        <div className="ChooseSetPage-box">
+                            <LearningSetBox title_and_date={"Arbeit Wortschatz"} />
+                        </div>
+                    </div>
+
+                    <div className="ChooseSetPage-begin-new-verb-rektion-column">
+                        <h5 className="ChooseSetPage-verb-rektion">Verb Rektion</h5>
+                        <div className="ChooseSetPage-box">
+                            <LearningSetBox title_and_date={"Arbeit Wortschatz"} />
+                        </div>
+                        <div className="ChooseSetPage-box">
+                            <LearningSetBox title_and_date={"Arbeit Wortschatz"} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
     </div>
+
 )
